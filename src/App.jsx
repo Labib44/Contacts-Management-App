eb2162074af56827a8131701d6b29527ec14daf0
Labib44 <mdlabib162@gmail.com>
2024-01-02 18:11:48 +0600

    Project setup done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className='text-4xl text-red-400'>project setup done</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
